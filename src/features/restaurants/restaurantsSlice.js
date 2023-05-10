a4abc199f460f8fcd3d48a5301270def4ed35d30
Andrew <andrewasmit@hotmail.com>
2023-05-10 11:54:42 -0500

    commit tree bcf247b51781b2791a122334d8f9b9828fb6b02b

--- src/features/restaurants/restaurantsSlice.js
+++ src/features/restaurants/restaurantsSlice.js
@@ -4,7 +4,19 @@ import { createSlice } from "@reduxjs/toolkit";
 const restaurantsSlice = createSlice({
   name: "restaurants",
   initialState: {},
-  reducers: {},
+  reducers: {
+    restaurantAdded(state, action){
+      state.entities.push({
+        name: action.payload,
+        id: uuid()
+      }) 
+    },
+    restaurantRemoved(state, action){
+      const idx = state.entities.findIndex(restaurant=>restaurant.id === action.payload);
+      state.entities.splice(idx, 1);
+    }
+  },
 });
 
 export default restaurantsSlice.reducer;
+export const { restaurantAdded, restaurantRemoved } = restaurantsSlice.actions;
